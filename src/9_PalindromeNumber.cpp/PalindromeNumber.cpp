4008f2d167068d47462d46a9c8649824028919ec
Stas Zvyagintsev <124485867+bulbasavr@users.noreply.github.com>
2024-02-26 11:19:14 +0300

    Add project files.

---
+++ src/9_PalindromeNumber.cpp/PalindromeNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        std::string str = std::to_string(x);
+        std::string strRevers = str;
+        std::reverse(strRevers.begin(), strRevers.end());
+
+        return str == strRevers;
+    }
+};
+
+void testsPalindrome()
+{
+    Solution solution;
+    assert(solution.isPalindrome(121) == true);
+    assert(solution.isPalindrome(-121) == false);
+    assert(solution.isPalindrome(10) == false);
+
+    std::cout << "Accepted" << std::endl;
+}
+
+int main()
+{
+    testsPalindrome();
+    std::cin.get();
+
+    return 0;
+}
