fbab998109a6397afa7eb0f621fcb760570a07e8
Stas Zvyagintsev <124485867+bulbasavr@users.noreply.github.com>
2024-02-29 00:11:29 +0300

    28. Find the Index of the First Occurrence in a String

---
+++ src/28_FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+class Solution
+{
+public:
+    int strStr(std::string haystack, std::string needle) { return haystack.find(needle); }
+};
+
+template <typename T>
+class Test
+{
+public:
+    void testInfo(T output, T expected)
+    {
+        if (output == expected)
+        {
+            std::cout << "[+] Output: " << output << "\tExpected: " << expected << std::endl;
+            return;
+        }
+
+        std::cout << "[-] Output: " << output << "\tExpected: " << expected << std::endl;
+    }
+
+    void simpleTest(T output, T expected)
+    {
+        testInfo(output, expected);
+        assert(output == expected);
+    }
+};
+
+void testsFindTheIndex()
+{
+    Solution solution;
+    Test<int> test;
+
+    test.simpleTest(solution.strStr("sadbutsad", "sad"), 0);
+    test.simpleTest(solution.strStr("leetcode", "leeto"), -1);
+
+    std::cout << "Accepted" << std::endl;
+}
+
+// int main()
+// {
+//     testsFindTheIndex();
+//     std::cin.get();
+//
+//     return 0;
+// }
