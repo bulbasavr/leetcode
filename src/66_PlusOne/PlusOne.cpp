df2c943549509b846872c07e290f8e965a5705f9
Stas Zvyagintsev <124485867+bulbasavr@users.noreply.github.com>
2024-03-02 05:12:29 +0300

    corrected a typo

--- src/66_PlusOne/PlusOne.cpp
+++ src/66_PlusOne/PlusOne.cpp
@@ -17,26 +17,30 @@ public:
 
         int lastElement = result.size() - 1;
 
-        if (lastElement < 9)
+        if (result[lastElement] < 9)
         {
             result[lastElement] += 1;
             return result;
         }
 
-        for (int i = lastElement; i > 1; i--)
+        for (int i = lastElement; i > -1; i--)
         {
-            if (result[i] - 1 == 9)
+            if (i != 0 && result[i - 1] == 9)
             {
                 result[i] = 0;
             }
-            else
+            else if (i != 0 && result[i - 1] != 9)
+            {
+                result[i] = 0;
+                result[i - 1] += 1;
+                break;
+            }
+            else if (i == 0 && result[i] == 9)
             {
                 result[i] = 1;
-                continue;
+                result.push_back(0);
             }
         }
-
-        result.push_back(0);
 
         return result;
     }
@@ -106,6 +110,10 @@ void testsPlusOne()
 
     test.input = {7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6};
     test.expected = {7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 7};
+    test.simpleTest(solution.plusOne(test.input), test.expected);
+
+    test.input = {8, 9, 9, 9};
+    test.expected = {9, 0, 0, 0};
     test.simpleTest(solution.plusOne(test.input), test.expected);
 
     std::cout << "Accepted" << std::endl;
